3a7bd61e59cb30bd2a58640f81c7d2a1379aaacf
mikeveit-ih <mike.veit@includehealth.com>
2025-02-28 10:49:41 -0500

    Initial Commit

---
+++ MacroTrack/MacroTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTrackApp.swift
+//  MacroTrack
+//
+//  Created by Mike Veit on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MacroTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
